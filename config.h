1c9d11f199447f22e8a8a8af616c539d1341b7c6
Pury <sondongxuan@gmail.com>
2024-03-28 15:40:53 +0700

    add bar height patch

--- config.h
+++ config.h
@@ -9,6 +9,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=11" };
 static const char dmenufont[]       = "monospace:size=11";
+static const int user_bh            = 27;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
